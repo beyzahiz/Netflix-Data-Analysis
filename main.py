9bed6bec10082f3d757d3523742e8aeffe29faac
Beyza Hız <beyzahiz.03@gmail.com>
2025-11-05 15:56:46 +0300

    data analysis

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ import pandas as pd
 df = pd.read_csv("netflix_titles.csv")
 dataframe = pd.DataFrame(df)
 
-print(df.head())
+#print(df.head())
 print(df.info())
+
+print("Veriseti boyutu: ", df.shape)
+print("Sütunlar: ",df.columns.tolist())
+print("Eksik veri sayısı:",df.isna().sum())
+print("Veri türleri: ",df.dtypes)
+
