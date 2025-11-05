1ccbefc635511998e432f8895a4ad16db8268009
Beyza Hız <beyzahiz.03@gmail.com>
2025-11-05 16:06:25 +0300

    filling NaN

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ df = pd.read_csv("netflix_titles.csv")
 dataframe = pd.DataFrame(df)
 
 #print(df.head())
-print(df.info())
+#print(df.info())
 
-print("Veriseti boyutu: ", df.shape)
+''' print("Veriseti boyutu: ", df.shape)
 print("Sütunlar: ",df.columns.tolist())
 print("Eksik veri sayısı:",df.isna().sum())
-print("Veri türleri: ",df.dtypes)
+print("Veri türleri: ",df.dtypes) '''
 
+df["director"] = df["director"].fillna("Unknown")
+df["cast"] = df["cast"].fillna("Unknown")
+df["country"] = df["country"].fillna("Unknown")
+df["date_added"] = df["date_added"].fillna("Unknown")
+df["rating"] = df["rating"].fillna("Unknown")
+df["duration"] = df["duration"].fillna("Unknown")
+print(df.isna().sum())
